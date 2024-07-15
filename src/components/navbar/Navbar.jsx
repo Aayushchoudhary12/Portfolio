b5f843bb4a8fff0e6f773b3c0c123c12b663facf
Aayush Choudhary <122475126+Aayushchoudhary12@users.noreply.github.com>
2024-07-15 22:44:34 +0530

    Update Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -15,13 +15,13 @@ export default function Navbar() {
                     animate={{ opacity: 1, scale: 1 }}
                     transition={{ duration: 0.5 }}
                 >
-                    Vidushi Arora
+                    Aayush Choudhary
                 </motion.span>
                 <div className="social">
-                    <a target="_blank" href="https://github.com/VidushiAr1">
+                    <a target="_blank" href="https://github.com/Aayushchoudhary12">
                         <img src="/github.png" alt="" />
                     </a>
-                    <a target="_blank" href="https://www.linkedin.com/in/vidushi-arora-841a21235">
+                    <a target="_blank" href="https://www.linkedin.com/in/aayush-choudhary-89620b22b">
                         <img src="/linkdin.png" alt="" />
                     </a>
                 </div>
